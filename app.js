8bab6e0bfb4deec0367a973a8b52515183fd7d16
amina-work <mina.works28@gmail.com>
2022-08-24 12:43:19 +0200

    progress point queries

---
+++ app.js
@@ -0,0 +1,21 @@
+const navItems = document.querySelector('.nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+// open nav dropdown
+const openNav = () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+// close nav dropdown
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+
+openNavBtn.addEventListener('click', openNav); //call the openNav function
+closeNavBtn.addEventListener('click', closeNav); //call the closeNav function
