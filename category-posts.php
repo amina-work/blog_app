6325642a2eda00ffefa64e7ba0bdaa6f510621d6
amina-work <mina.works28@gmail.com>
2022-08-28 21:02:17 +0200

    prog point bug at php, file content disappear when adding <?= ROOT_URL ?> to file links created header & footer files for shared content

--- category-posts.php
+++ category-posts.php
@@ -1,49 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Multipage Blog Application</title>
-    
-    <!-- SASS Style-->
-    <link rel="stylesheet" href="./styles/styles.css">
-
-    <!-- Iconscout cdn-->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
-
-    <!-- Montserrat Font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&family=Outfit:wght@400;700&display=swap" rel="stylesheet">
-
-</head>
-<body>
-    <!-- Navigation Bar -->
-    <nav class="nav">
-        <div class="container nav__container">
-            <a href="index.html" class="nav__logo">Blogs.</a>
-            <ul class="nav__items">
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <!--<li><a href="signin.html">Sign in</a></li>-->
-                <li class="nav__profile">
-                    <div class="avatar">
-                        <img src="./images/avatar1.png" alt="">
-                    </div>
-                    <ul>
-                        <li><a href="dashboard.html">Dashboard</a></li>
-                        <li><a href="logout.html">Log Out</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
-            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
-        </div>
-    </nav>
+<?php
+include 'partials/header.php'
+?>
 
     <!--Category title-->
     <header class="category__title">
