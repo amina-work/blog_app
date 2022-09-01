addd4445a3d8c7c15ab0ed6e4fc139deca40cc1a
amina-work <mina.works28@gmail.com>
2022-09-01 14:46:04 +0200

    manage-users DB finished admin have a full catalogue of all added users in the manage-users page sauf themselves they will display their first & last name, username, an edit & delete option & their admin status each user will be tested whether they are admin or not & display accordignly

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -1,5 +1,11 @@
 <?php 
 include 'partials/header.php';
+
+//fetch users from DB but not current user
+$current_admin_id = $_SESSION['user-id'];
+
+$query = "SELECT * FROM users WHERE NOT id=$current_admin_id";
+$users = mysqli_query($connection, $query);
 ?>
 
 
@@ -75,13 +81,17 @@ include 'partials/header.php';
                         </tr>
                     </thead>
                     <tbody>
+                        <?php while($user = mysqli_fetch_assoc($users)) : ?>
                         <tr>
-                            <td>Ernest Frowl</td>
-                            <td>ufrowl</td>
-                            <td><a href="edit-user.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                            <td>Yes</td>
+                            <td><?= "{$user['firstname']} {$user['lastname']}" ?></td>
+                            <td><?= "{$user['username']}" ?></td>
+                            <td><a href="<?= ROOT_URL ?>admin/edit-user.php?id=<?= $user['id'] ?>" class="btn sm">Edit</a></td>
+                            <td><a href="<?= ROOT_URL ?>admin/delete-user.php?id=<?= $user['id'] ?>" class="btn sm danger">Delete</a></td>
+                            <td>
+                                <?= $user['is_admin'] ? "yes" : "no" ?>
+                            </td>
                         </tr> <!--End item 1-->
+                        <?php endwhile ?>
                     </tbody>
                 </table>
             </main>
