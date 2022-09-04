1535ddcbd7b3a59cc8fa609e264f5fab0b7f9686
amina-work <mina.works28@gmail.com>
2022-09-04 17:42:16 +0200

    posts DB prog users can now fetch posts in the manage posts page each user will only be shown just his own posts the post chosen category will also be shown

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,10 @@
 <?php 
 include 'partials/header.php';
+
+//fetch current user posts only
+$current_user_id = $_SESSION['user-id'];
+$query = "SELECT id, title, category_id FROM posts WHERE author_id=$current_user_id ORDER BY id DESC";
+$posts = mysqli_query($connection, $query);
 ?>
 
     <!--Dashboard-->
@@ -63,48 +68,38 @@ include 'partials/header.php';
             </aside>
             <main>
                 <h2>Manage Posts</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur elit.</td>
-                            <td>Wild Life</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                        </tr> <!--End item 1-->
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                            <td>Art</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                        </tr> <!--End item 2-->
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                            <td>Science & Tecknology</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                        </tr> <!--End item 3-->
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                            <td>Art</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                        </tr> <!--End item 4-->
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
-                            <td>Travail</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-category.php" class="btn sm danger">Delete</a></td>
-                        </tr> <!--End item 5-->
-                    </tbody>
-                </table>
+                <?php if(mysqli_num_rows($posts) > 0) : ?>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while($post = mysqli_fetch_assoc($posts)) : ?>
+                                <?php 
+                                $category_id = $post['category_id'];
+                                $category_query = "SELECT title FROM categories WHERE id=$category_id";
+                                $category_result = mysqli_query($connection, $category_query);
+                                $category = mysqli_fetch_assoc($category_result);
+                                ?>
+                                <tr>
+                                    <td><?= $post['title'] ?></td>
+                                    <td><?= $category['title'] ?></td>
+                                    <td><a href="<?= ROOT_URL ?>admin/edit-post.php?id=<?= $post['id'] ?>" class="btn sm">Edit</a></td>
+                                    <td><a href="<?= ROOT_URL ?>admin/delete-post.php?id=<?= $post['id'] ?>" class="btn sm danger">Delete</a></td>
+                                </tr> <!--End item 1-->
+                            <?php endwhile ?>
+                        </tbody>
+                    </table>
+                <?php else : ?>
+                    <div class="alert__message error">
+                        <p><?= "No posts found" ?></p>
+                    </div>
+                <?php endif ?>
             </main>
         </div>
     </section>
