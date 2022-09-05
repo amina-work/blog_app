97651a02e85976f659c95ec4ece5107e472c2401
amina-work <mina.works28@gmail.com>
2022-09-05 22:28:44 +0200

    home page prog started with the index page dynamic section now featured post is in featured section it shows the thumbnail, title, 250 of body len, author name & avatar, date & time of post release remains showing the rest of the posts, 8 posts in home page & all posts in blogs page

--- index.php
+++ index.php
@@ -1,31 +1,50 @@
 <?php
-include 'partials/header.php'
+include 'partials/header.php';
+
+//fetch featured post from DB
+$featured_query = "SELECT * FROM posts WHERE is_featured=1";
+$featured_result = mysqli_query($connection, $featured_query);
+$featured = mysqli_fetch_assoc($featured_result);
 ?>
+
+<?php if(mysqli_num_rows($featured_result) == 1) : ?>
     <!-- Feautured Section -->
     <section class="featured">
         <div class="container featured__container">
             <div class="post__thumbail">
-                <img src="./images/post1.jpeg" alt="">
+                <img src="./images/<?= $featured['thumbnail'] ?>">
             </div>
             <div class="post__info">
-                <a class="category__button" href="category-posts.php">Wild Life</a>
-                <h2 class="post__title"><a href="post.php">How to stop friends from sending voice messages all the time</a></h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum illo distinctio eveniet itaque
-                    sunt nihil tempora nesciunt accusantium alias esse obcaecati.
-                </p>
+                <?php
+                //Fetch category from category_id of post
+                $category_id = $featured['category_id'];
+                $category_query = "SELECT * FROM categories WHERE id=$category_id";
+                $category_result = mysqli_query($connection, $category_query);
+                $category = mysqli_fetch_assoc($category_result);
+                ?>
+                <a class="category__button" href="<?= ROOT_URL ?>category-posts.php?id=<?= $category['id'] ?>"><?= $category['title'] ?></a>
+                <h2 class="post__title"><a href="<?= ROOT_URL ?>post.php?id=<?= $featured['id'] ?>"><?= $featured['title'] ?></a></h2>
+                <p><?= substr($featured['body'], 0, 250) ?>...</p>
                 <div class="post__author">
+                    <?php
+                    //fetch author from users table by using author_id
+                    $author_id = $featured['author_id'];
+                    $author_query = "SELECT * FROM users WHERE id=$author_id";
+                    $author_result = mysqli_query($connection, $author_query);
+                    $author = mysqli_fetch_assoc($author_result);
+                    ?>
                     <div class="post__author-avatar">
-                        <img src="./images/avatar1.png" alt="">
+                        <img src="./images/<?= $author['avatar'] ?>" alt="">
                     </div>
                     <div class="post__author-info">
-                        <h5>By: John Stone</h5>
-                        <small>June 10, 2022 - 7:40</small>
+                        <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+                        <small><?= date("M d, Y - H:i", strtotime($featured['date_time'])) ?></small>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+<?php endif ?>
     
     <!-- Posts Section -->
     <section class="posts">
